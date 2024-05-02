27adbebef92c1533201455a47f46b0234ed47b7a
srinadhreddy <srinadhreddy@yupptv.com>
2024-05-02 20:05:55 +0530

    small change

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const ibmPlexSerif = IBM_Plex_Serif({
 });
 
 export const metadata: Metadata = {
-  title: "Bank",
-  description: "Bank is a modern banking platform for everyone.",
+  title: "Horizon Bank",
+  description: "Horizon Bank is a modern banking platform for everyone.",
   icons: {
     icon: "/icons/logo.svg",
   },
